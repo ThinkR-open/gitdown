279ebb8e22fc7ffc249e8392a9980523cd413b62
Sébastien Rochette <sebastienrochettefr@gmail.com>
2020-08-07 19:50:47 +0200

    Allow pattern.table in git_down

--- tests/testthat/test-git_down.R
+++ tests/testthat/test-git_down.R
@@ -11,3 +11,18 @@ test_that("git_down function",{
     expect_match(res, regexp = ".html")
   })
 })
+
+# With table of correspondance
+pattern.table <- data.frame(number = c("#2", "#1"),
+  title = c("#2 A second issue to illustrate a blog post",
+                       "#1 An example of issue"))
+
+test_that("git_down with pattern table",{
+  with_dir(repo, {
+    res <- git_down(author = "Cervan",
+                    pattern = c("Issue" = "#[[:digit:]]+"),
+                    pattern.table = pattern.table
+    )
+    expect_match(res, regexp = ".html")
+  })
+})
